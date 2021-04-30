5c1b2962dc20c1167677dc2232649510f2fe23bd
wurining <wurining@gmail.com>
2021-04-30 16:58:24 +0100

    feature - add new conf - add a method for setting alpha and beta value in the main function. The first agrs is Path of conf, the second is alpha, the third is beta.

--- src/main/java/experiment/IEPOSExperiment.java
+++ src/main/java/experiment/IEPOSExperiment.java
@@ -31,133 +31,134 @@ import protopeer.util.quantities.Time;
 import treestructure.ModifiableTreeArchitecture;
 
 /**
- * 
- * 
  * @author Jovan N., Thomas Asikis
- *
  */
 public class IEPOSExperiment {
 
-	public static void runSimulation(int numChildren, // number of children for each middle node
-			int numIterations, // total number of iterations to run for
-			int numAgents, // total number of nodes in the network
-			Function<Integer, Agent> createAgent, // lambda expression that creates an agent
-			Configuration config) {
+    public static void runSimulation(int numChildren, // number of children for each middle node
+                                     int numIterations, // total number of iterations to run for
+                                     int numAgents, // total number of nodes in the network
+                                     Function<Integer, Agent> createAgent, // lambda expression that creates an agent
+                                     Configuration config) {
 
-		SimulatedExperiment experiment = new SimulatedExperiment() {
-		};
-		ModifiableTreeArchitecture architecture = new ModifiableTreeArchitecture(config);
+        SimulatedExperiment experiment = new SimulatedExperiment() {
+        };
+        ModifiableTreeArchitecture architecture = new ModifiableTreeArchitecture(config);
 
-		SimulatedExperiment.initEnvironment();
-		experiment.init();
-		PeerFactory peerFactory = new PeerFactory() {
-			@Override
-			public Peer createPeer(int peerIndex, Experiment e) {
-				Agent newAgent = createAgent.apply(peerIndex);
-				Peer newPeer = new Peer(peerIndex);
+        SimulatedExperiment.initEnvironment();
+        experiment.init();
+        PeerFactory peerFactory = new PeerFactory() {
+            @Override
+            public Peer createPeer(int peerIndex, Experiment e) {
+                Agent newAgent = createAgent.apply(peerIndex);
+                Peer newPeer = new Peer(peerIndex);
 
-				architecture.addPeerlets(newPeer, newAgent, peerIndex, numAgents);
+                architecture.addPeerlets(newPeer, newAgent, peerIndex, numAgents);
 
-				return newPeer;
-			}
-		};
+                return newPeer;
+            }
+        };
 
-		Logger rootLogger = LogManager.getLogManager().getLogger("");
-		rootLogger.setLevel(config.loggingLevel);
-		for (Handler h : rootLogger.getHandlers()) {
-			h.setLevel(config.loggingLevel);
-			h.setFormatter(new CustomFormatter());
-		}
+        Logger rootLogger = LogManager.getLogManager().getLogger("");
+        rootLogger.setLevel(config.loggingLevel);
+        for (Handler h : rootLogger.getHandlers()) {
+            h.setLevel(config.loggingLevel);
+            h.setFormatter(new CustomFormatter());
+        }
 
-		experiment.initPeers(0, numAgents, peerFactory);
-		experiment.startPeers(0, numAgents);
-		experiment.runSimulation(Time.inSeconds(3 + numIterations));
-	}
+        experiment.initPeers(0, numAgents, peerFactory);
+        experiment.startPeers(0, numAgents);
+        experiment.runSimulation(Time.inSeconds(3 + numIterations));
+    }
 
-	private static void runOneSimulation(Configuration config, Function<Integer, Agent> createAgent) {
-		long timeBefore = System.currentTimeMillis();
-		IEPOSExperiment.runSimulation(Configuration.numChildren, Configuration.numIterations,
-				Configuration.numAgents, createAgent, config);
-		long timeAfter = System.currentTimeMillis();
-		System.out.println("IEPOS Finished! It took: " + ((timeAfter - timeBefore) / 1000) + " seconds.");
-	}
-
-
-	public static void main(String[] args) {
-		Logger log = Logger.getLogger(IEPOSExperiment.class.getName());
-
-		String confPath = null;
-
-		if (args.length > 0) {
-			Path pathURI = Paths.get(args[0]);
-
-			if (!Files.notExists(pathURI) && Files.isRegularFile(pathURI)) {
-				log.log(Level.INFO, "Configuration file path provided from command line, "
-						+ "overriding default conf location and using file in: \n" + pathURI.toString());
-				confPath = pathURI.toString();
-			}
-
-		}
-
-		String rootPath = System.getProperty("user.dir");
-
-		confPath = confPath == null ? rootPath + File.separator + "conf" + File.separator + "epos.properties"
-				: confPath;
+    private static void runOneSimulation(Configuration config, Function<Integer, Agent> createAgent) {
+        long timeBefore = System.currentTimeMillis();
+        IEPOSExperiment.runSimulation(Configuration.numChildren, Configuration.numIterations,
+                Configuration.numAgents, createAgent, config);
+        long timeAfter = System.currentTimeMillis();
+        System.out.println("IEPOS Finished! It took: " + ((timeAfter - timeBefore) / 1000) + " seconds.");
+    }
 
 
-		Configuration config = Configuration.fromFile(confPath);
-		config.printConfiguration();
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(IEPOSExperiment.class.getName());
+
+        String confPath = null;
+
+        if (args.length > 0) {
+            Path pathURI = Paths.get(args[0]);
+
+            if (!Files.notExists(pathURI) && Files.isRegularFile(pathURI)) {
+                log.log(Level.INFO, "Configuration file path provided from command line, "
+                        + "overriding default conf location and using file in: \n" + pathURI.toString());
+                confPath = pathURI.toString();
+            }
+
+        }
+
+        String rootPath = System.getProperty("user.dir");
+
+        confPath = confPath == null ? rootPath + File.separator + "conf" + File.separator + "epos.properties"
+                : confPath;
 
 
-		LoggingProvider<MultiObjectiveIEPOSAgent<Vector>> loggingProvider = new LoggingProvider<>();
-		
-		for (AgentLogger logger : config.loggers) {
-			loggingProvider.add(logger);
-		}
+        Configuration config = Configuration.fromFile(confPath);
+        // set args[1] => a, set args[2]=>b
+        if (args.length > 2) {
+            config.weights = new String[]{args[1], args[2]};
+        }
+        config.printConfiguration();
 
-		for (int sim = 0; sim < Configuration.numSimulations; sim++) {
 
-			System.out.println("Simulation " + (sim + 1));
+        LoggingProvider<MultiObjectiveIEPOSAgent<Vector>> loggingProvider = new LoggingProvider<>();
 
-			final int simulationId = sim;
-			config.permutationSeed = sim;
+        for (AgentLogger logger : config.loggers) {
+            loggingProvider.add(logger);
+        }
 
-			for (AgentLogger al : loggingProvider.getLoggers()) {
-				al.setRun(sim);
-			}
+        for (int sim = 0; sim < Configuration.numSimulations; sim++) {
 
-			if (Configuration.numSimulations > 1 && sim > 0) {
-				Configuration.mapping = config.generateMappingForRepetitiveExperiments.apply(config);
-			}
+            System.out.println("Simulation " + (sim + 1));
 
-			PlanSelector<MultiObjectiveIEPOSAgent<Vector>, Vector> planSelector = new MultiObjectiveIeposPlanSelector<Vector>();
+            final int simulationId = sim;
+            config.permutationSeed = sim;
 
-			/**
-			 * Function that creates an Agent given the id of it's vertex in tree graph.
-			 * First type is input argument, second type is type of return value.
-			 */
-			Function<Integer, Agent> createAgent = agentIdx -> {
+            for (AgentLogger al : loggingProvider.getLoggers()) {
+                al.setRun(sim);
+            }
 
-				List<Plan<Vector>> possiblePlans = config.getDataset(Configuration.dataset)
-						.getPlans(Configuration.mapping.get(agentIdx));
-				AgentLoggingProvider<ModifiableIeposAgent<Vector>> agentLP = loggingProvider
-						.getAgentLoggingProvider(agentIdx, simulationId);
+            if (Configuration.numSimulations > 1 && sim > 0) {
+                Configuration.mapping = config.generateMappingForRepetitiveExperiments.apply(config);
+            }
 
-				ModifiableIeposAgent<Vector> newAgent = new ModifiableIeposAgent<Vector>(config, possiblePlans,
-						agentLP);
+            PlanSelector<MultiObjectiveIEPOSAgent<Vector>, Vector> planSelector = new MultiObjectiveIeposPlanSelector<Vector>();
 
-				newAgent.setUnfairnessWeight(Double.parseDouble(config.weights[0]));
-				newAgent.setLocalCostWeight(Double.parseDouble(config.weights[1]));
-				newAgent.setPlanSelector(planSelector);
-				return newAgent;
+            /**
+             * Function that creates an Agent given the id of it's vertex in tree graph.
+             * First type is input argument, second type is type of return value.
+             */
+            Function<Integer, Agent> createAgent = agentIdx -> {
 
-			};
+                List<Plan<Vector>> possiblePlans = config.getDataset(Configuration.dataset)
+                        .getPlans(Configuration.mapping.get(agentIdx));
+                AgentLoggingProvider<ModifiableIeposAgent<Vector>> agentLP = loggingProvider
+                        .getAgentLoggingProvider(agentIdx, simulationId);
 
-			IEPOSExperiment.runOneSimulation(config, createAgent);
-		}
+                ModifiableIeposAgent<Vector> newAgent = new ModifiableIeposAgent<Vector>(config, possiblePlans,
+                        agentLP);
 
-		loggingProvider.print();
+                newAgent.setUnfairnessWeight(Double.parseDouble(config.weights[0]));
+                newAgent.setLocalCostWeight(Double.parseDouble(config.weights[1]));
+                newAgent.setPlanSelector(planSelector);
+                return newAgent;
 
-	}
+            };
+
+            IEPOSExperiment.runOneSimulation(config, createAgent);
+        }
+
+        loggingProvider.print();
+
+    }
 
 }
